12906661a67f1528a872c6c5564e1bbd1dab56ba
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-07-11 02:02:59 +0300

    updated files

--- app/src/main/java/com/example/finalspace/FindActivity.java
+++ app/src/main/java/com/example/finalspace/FindActivity.java
@@ -15,23 +15,30 @@ import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class FindActivity extends AppCompatActivity implements View.OnClickListener {
-    private String[] seasons = new String[]{
+public class FindActivity extends AppCompatActivity {
+    @BindView(R.id.list) ListView mListView;
+    private String[] Seasons = new String[]{
             "Season one", "Season two", "Season three"
     };
-
-    @BindView(R.id.list) ListView mListView;
-    @BindView(R.id.button2) Button mCheckEpisodes;
-
+    private String[] Chapters = new String[] {
+            "Chapter One", "Chapter Two", "Chapter Three"
+    };
+//    private String[] Scenes = new String[]{
+//            "The Toro Regetta", "The Happy Place", "The Grand Surrender", "The Other Side", "The Notorious Mrs.Goodspeed",
+//            "Arachnitects", "The First Times They Met", "The Remembered", "The Closer You Get", "The Lost Spy"
+//    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find);
         ButterKnife.bind(this);
-        mCheckEpisodes.setOnClickListener(this);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, seasons);
+        Intent intent = getIntent();
+        String find = intent.getStringExtra("find");
+
+        FindArrayAdapter adapter = new FindArrayAdapter(this, android.R.layout.simple_list_item_1, Seasons, Chapters);
         mListView.setAdapter(adapter);
+
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -41,11 +48,4 @@ public class FindActivity extends AppCompatActivity implements View.OnClickListe
         });
     }
 
-    @Override
-    public void onClick(View v) {
-        if (v == mCheckEpisodes){
-            Intent intent = new Intent(FindActivity.this, EpisodeActivity.class);
-            startActivity(intent);
-        }
-    }
 }
--- app/src/main/java/com/example/finalspace/FindArrayAdapter.java
+++ app/src/main/java/com/example/finalspace/FindArrayAdapter.java
@@ -5,26 +5,26 @@ import android.widget.ArrayAdapter;
 
 public class FindArrayAdapter extends ArrayAdapter {
     private Context mContext;
+    private String[] mSeasons;
     private String[] mChapters;
-    private String[] mScenes;
 
-    public FindArrayAdapter(Context mContext, int resources, String[] mChapters, String[] mScenes){
+    public FindArrayAdapter(Context mContext, int resources, String[] mSeasons, String[] mChapters){
         super(mContext, resources);
         this.mContext = mContext;
+        this.mSeasons = mSeasons;
         this.mChapters = mChapters;
-        this.mScenes = mScenes;
 
     }
 
     @Override
     public Object getItem(int position){
+        String season = mSeasons[position];
         String chapter = mChapters[position];
-        String scene = mScenes[position];
-        return String.format("%s \nServes great : %s", chapter, scene);
+        return String.format("%s \nServes great : %s",season, chapter);
     }
     @Override
     public int getCount(){
-        return mChapters.length;
+        return mSeasons.length;
     }
 
 }
