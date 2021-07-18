b4ba0dee832d2a7c7479aad25137bfa9e27e310a
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-07-18 20:57:43 +0300

    updated files

--- app/src/main/java/com/queen/finalspace/model/Quote.java
+++ app/src/main/java/com/queen/finalspace/model/Quote.java
@@ -1,9 +1,12 @@
 
-package com.example.finalspace.model;
+package com.queen.finalspace.model;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import org.parceler.Parcel;
+
+@Parcel
 public class Quote {
 
     @SerializedName("id")
